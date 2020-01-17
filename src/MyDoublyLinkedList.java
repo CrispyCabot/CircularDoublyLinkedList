ad52418e9d5cfe473c88c1f48d4c1fe594f60657
CrispyCabot <18431358+CrispyCabot@users.noreply.github.com>
2020-01-16 19:10:08 -0500

    other stuff

--- src/MyDoublyLinkedList.java
+++ src/MyDoublyLinkedList.java
@@ -1,4 +1,5 @@
 import java.util.ListIterator;
+import java.util.NoSuchElementException;
 
 public class MyDoublyLinkedList<E> extends MyAbstractSequentialList<E>
         implements Cloneable {
@@ -9,6 +10,40 @@ public class MyDoublyLinkedList<E> extends MyAbstractSequentialList<E>
         head = new Node(null);
         head.previous = head;
         head.next = head;
+    }
+
+  /*  @Override
+    public boolean equals(Object list) {
+        if (!(list instanceof MyList)) {
+            return false;
+        }
+        Node<E> current = head;
+        for (int i=-1; i<size; i++) {
+            if (!(current.element.equals(list.get(i))))
+                return false;
+        }
+    } */
+    @Override
+    public String toString() {
+        String str = "[";
+        Node<E> current = head.next;
+        for (int i=0; i<size-1; i++) { //Does last element outside loop
+            str += current.element.toString()+", ";
+            current = current.next;
+        }
+        str += current.element.toString()+"]";
+        return str;
+    }
+
+    @Override
+    public Object clone() {
+        MyDoublyLinkedList newList = new MyDoublyLinkedList();
+        Node<E> current = head.next;
+        for (int i=0; i<size; i++) {
+            newList.addLast(current.element);
+            current = current.next;
+        }
+        return newList;
     }
 
     @Override
@@ -41,6 +76,9 @@ public class MyDoublyLinkedList<E> extends MyAbstractSequentialList<E>
 
     @Override
     public E removeFirst() {
+        if (size<=0) {
+            throw new NoSuchElementException();
+        }
         E temp = head.next.element;
         head.next = head.next.next;
         size--;
@@ -49,6 +87,9 @@ public class MyDoublyLinkedList<E> extends MyAbstractSequentialList<E>
 
     @Override
     public E removeLast() {
+        if (size<=0) {
+            throw new NoSuchElementException();
+        }
         E temp = head.previous.element;
         head.previous = head.previous.previous;
         size--;
@@ -68,8 +109,8 @@ public class MyDoublyLinkedList<E> extends MyAbstractSequentialList<E>
             current = current.next;
         }
         addedElement.next = current.next;
-        addedElement.previous = current;
         current.next = addedElement;
+        addedElement.previous = current;
         size++;
     }
 
@@ -82,6 +123,12 @@ public class MyDoublyLinkedList<E> extends MyAbstractSequentialList<E>
 
     @Override
     public boolean contains(E e) {
+        Node<E> current = head.next;
+        for (int i=0; i<size; i++) {
+            if (e.equals(current.element))
+                return true;
+            current = current.next;
+        }
         return false;
     }
 
@@ -90,27 +137,69 @@ public class MyDoublyLinkedList<E> extends MyAbstractSequentialList<E>
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException();
         }
-        return head.getNode(index, head, size).element;
+        return getNode(index).element;
     }
 
     @Override
     public int indexOf(E e) {
-        return 0;
+        Node<E> current = head.next;
+        for (int i=0; i<size; i++) {
+            if (e.equals(current)) {
+                return i;
+            }
+            current = current.next;
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(E e) {
-        return 0;
+        int index = -1;
+        Node<E> current = head.next;
+        for (int i=0; i<size; i++) {
+            if (e.equals(current)) {
+                index = i;
+            }
+            current = current.next;
+        }
+        return index;
     }
 
     @Override
     public E remove(int index) {
-        return null;
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        E val = getNode(index).element;
+        Node<E> current = head;
+        for (int i=0; i<index; i++) {
+            current = current.next;
+        }
+        current.next = current.next.next;
+        return val;
     }
 
     @Override
     public Object set(int index, E e) {
-        return null;
+        E val = getNode(index).element;
+        Node<E> current = head.next;
+        for (int i=0; i<size; i++) {
+            current = current.next;
+        }
+        current.element = e;
+        return val;
+    }
+
+    private Node<E> getNode(int index) {
+        Node<E> current = head;
+        if (index < size / 2) {
+            for (int i = -1; i < index; i++)
+                current = current.next;
+        } else {
+            for (int i = size; i > index; i--)
+                current = current.previous;
+        }
+        return current;
     }
 
     private static class Node<E> {
@@ -120,26 +209,6 @@ public class MyDoublyLinkedList<E> extends MyAbstractSequentialList<E>
 
         public Node(E element) {
             this.element = element;
-        }
-
-        /*
-                private Node<E> getNode(int index) {
-                    Node<E> current = head;
-                    for (int i=-1; i<index; i++)
-                        current = current.next;
-                    return current;
-                }
-                */
-        private Node<E> getNode(int index, Node<E> head, int size) {
-            Node<E> current = head;
-            if (index < size / 2) {
-                for (int i = -1; i < index; i++)
-                    current = current.next;
-            } else {
-                for (int i = size; i > index; i--)
-                    current = current.previous;
-            }
-            return current;
         }
     }
 }
